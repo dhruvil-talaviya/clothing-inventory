18c8a3b4b69095ac31995eb52708bd1bd9330c16
dhruvil talaviya <dhruviltalaviya@dhruvils-MacBook-Air.local>
2026-04-01 15:43:34 +0530

    update API URL to production

--- fronted/src/config.js
+++ fronted/src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 const config = {
     // This is the URL of your backend server
-    API_BASE_URL: "http://localhost:5001/api"
+    API_BASE_URL: "https://clothing-inventory-bbhg.onrender.com/api"
 };
 
 export default config;
